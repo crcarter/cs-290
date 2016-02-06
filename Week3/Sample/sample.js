d869d28193b40ffa0add104d7dd54dbd00702241
crcarter <crcarter07@gmail.com>
2016-02-05 16:24:47 -0800

    Week 3 Content
    DeepEqual and sample

--- Week3/Sample/sample.js
+++ Week3/Sample/sample.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+console.log("Hello World!");console.log("Lets make sure JavaScript is working.");
+var name = "Christian"; //Replace this with your first name
+console.log("The unicode characters of your name are:")
+for (var i = 0; i < name.length; i++){
+	console.log(name.charCodeAt(i));
+}
+console.log("Copy and paste these values for activity credit.")
+
+console.log(triple(3));
+
+function triple( num ) {
+	return num * 3;
+};
+
+console.log(cube(4));
+
+var cube = function( num ) {
+	return num * num * num;
+};
